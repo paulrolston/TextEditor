bb5d58928ecb1fd5c0c1a7f8ff67e921d1d73ed1
Paul Rolston <prolston35@icloud.com>
2026-09-22 21:46:59 +0100

    Displaying file in tool bar

--- src/main.c
+++ src/main.c
@@ -162,6 +162,15 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     SDL_RenderFillRect(renderer, &tool_bar);
     SDL_SetRenderDrawColor(renderer, 200,200,220,255);
     SDL_RenderRect(renderer, &tool_bar);
+    // SDL_SetRenderDrawColor(renderer, 240,240,240,255);
+    char text[100] = "File: \0";
+    strcat(text, text_window->data->file_path);
+    SDL_Surface* file_surface = TTF_RenderText_Shaded(font,text,0,text_window->foreground,(SDL_Color){0,0,0,SDL_ALPHA_TRANSPARENT});
+    SDL_Texture* file_tex = SDL_CreateTextureFromSurface(renderer, file_surface);
+    SDL_DestroySurface(file_surface);
+    SDL_FRect file_rect = {tool_bar.x,tool_bar.y};
+    SDL_GetTextureSize(file_tex,&file_rect.w,&file_rect.h);
+    SDL_RenderTexture(renderer, file_tex,NULL,&file_rect);
     //draw the text window
     draw_window(renderer, font, text_window);
     SDL_RenderPresent(renderer);
