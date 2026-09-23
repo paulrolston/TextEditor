532a799d3b5b2d3a73e6143235cfa936d927d29d
Paul Rolston <prolston35@icloud.com>
2026-09-23 22:39:52 +0100

    Updated button header

--- include/button.h
+++ include/button.h
@@ -11,6 +11,7 @@
 typedef struct Button {
     SDL_FRect button_r;
     SDL_FRect text_r;
+    SDL_Texture* text_t;
     char text[25];
     SDL_Color background, border, foreground;
     bool hovered;
@@ -20,7 +21,7 @@ typedef struct Button {
 /*
 Create a button object, pass coordinates in unscaled values, i.e raw numbers.
 */
-Button* create_button(SDL_Renderer* r, TTF_Font* font, const char* text, float x, float y, float w, float h);
+Button* create_button(SDL_Renderer* r, TTF_Font* font, const char* text, float x, float y, float w, float h, SDL_Color background, SDL_Color border, SDL_Color foreground);
 
 //Draws the button, automatically scales with high DPI
 void draw_button(SDL_Renderer* r, Button* b);
