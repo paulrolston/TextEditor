89216b1f1bfcd0f55fc5834240505444c4cd99d8
Paul Rolston <prolston35@icloud.com>
2026-09-24 18:12:59 +0100

    Start of toast manager header

---
+++ include/toastmanager.h
@@ -0,0 +1,27 @@
+#ifndef TOASTMANAGER_H
+#define TOASTMANAGER_H
+
+#define MAX_TOASTS 8
+
+#include "globals.h"
+#include "toast.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct ToastManager {
+    UIToast* toasts;
+    size_t num_toasts;
+} ToastManager;
+
+ToastManager* create_toast_manager();
+//update all of the toasts
+void update_toast_manager(SDL_Renderer* renderer, TTF_Font font, ToastManager* manager);
+//draw the toasts
+void draw_toast_manager(SDL_Renderer* renderer, ToastManager* manager);
+void new_toast(UIToast toast);
+
+#endif // TOASTMANAGER.H_H
